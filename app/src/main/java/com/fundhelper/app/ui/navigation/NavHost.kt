fc593e4a89ace10002b580c472af6d07261edfb7
Obrige <174951221+obrige@users.noreply.github.com>
2026-05-17 00:08:31 +0800

    fix: URL-encode nav args, simplify IndexDetail route to secId/name/code only

--- app/src/main/java/com/fundhelper/app/ui/navigation/NavHost.kt
+++ app/src/main/java/com/fundhelper/app/ui/navigation/NavHost.kt
@@ -7,24 +7,25 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.fundhelper.app.data.model.IndexDisplayItem
-import com.fundhelper.app.data.model.IndexEntity
-import com.fundhelper.app.data.model.IndexQuoteItem
 import com.fundhelper.app.ui.home.HomeScreen
 import com.fundhelper.app.ui.detail.FundDetailScreen
 import com.fundhelper.app.ui.detail.IndexDetailScreen
 import com.fundhelper.app.ui.market.MarketCenterScreen
 import com.fundhelper.app.ui.settings.SettingsScreen
+import java.net.URLEncoder
+import java.net.URLDecoder
 
 sealed class Screen(val route: String) {
     data object Home : Screen("home")
     data object FundDetail : Screen("fund_detail/{code}") {
         fun createRoute(code: String) = "fund_detail/$code"
     }
-    data object IndexDetail : Screen("index_detail/{secId}/{name}/{code}/{market}/{price}/{changeRate}/{change}") {
+    data object IndexDetail : Screen("index_detail/{secId}/{name}/{code}") {
         fun createRoute(item: IndexDisplayItem): String {
-            val q = item.quote
-            return "index_detail/${item.entity.secId}/${item.entity.name}/${item.entity.code}" +
-                    "/${item.entity.market}/${q?.price ?: 0}/${q?.changeRate ?: 0}/${q?.change ?: 0}"
+            val sid = URLEncoder.encode(item.entity.secId, "UTF-8")
+            val nm = URLEncoder.encode(item.entity.name, "UTF-8")
+            val cd = URLEncoder.encode(item.entity.code, "UTF-8")
+            return "index_detail/$sid/$nm/$cd"
         }
     }
     data object MarketCenter : Screen("market_center")
@@ -61,33 +62,17 @@ fun FundNavHost() {
             arguments = listOf(
                 navArgument("secId") { type = NavType.StringType },
                 navArgument("name") { type = NavType.StringType },
-                navArgument("code") { type = NavType.StringType },
-                navArgument("market") { type = NavType.IntType },
-                navArgument("price") { type = NavType.FloatType },
-                navArgument("changeRate") { type = NavType.FloatType },
-                navArgument("change") { type = NavType.FloatType }
+                navArgument("code") { type = NavType.StringType }
             )
         ) { backStackEntry ->
             val args = backStackEntry.arguments!!
-            val item = IndexDisplayItem(
-                entity = IndexEntity(
-                    secId = args.getString("secId")!!,
-                    name = args.getString("name")!!,
-                    code = args.getString("code")!!,
-                    market = args.getInt("market")
-                ),
-                quote = IndexQuoteItem(
-                    price = args.getDouble("price"),
-                    changeRate = args.getDouble("changeRate"),
-                    change = args.getDouble("change"),
-                    code = args.getString("code")!!,
-                    market = args.getInt("market"),
-                    name = args.getString("name")!!,
-                    amount = null
-                )
-            )
+            val secId = URLDecoder.decode(args.getString("secId") ?: "", "UTF-8")
+            val name = URLDecoder.decode(args.getString("name") ?: "", "UTF-8")
+            val code = URLDecoder.decode(args.getString("code") ?: "", "UTF-8")
             IndexDetailScreen(
-                item = item,
+                secId = secId,
+                name = name,
+                code = code,
                 onBack = { navController.popBackStack() }
             )
         }
